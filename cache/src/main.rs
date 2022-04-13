a897b448a03f9ffd257708d44d6524ac2c800909
Cory Tomlinson <corytomlinson6640@gmail.com>
2022-04-13 09:21:19 -0600

    added args processing

--- cache/src/main.rs
+++ cache/src/main.rs
@@ -1,14 +1,30 @@
+#![allow(warnings, unused)]
+
 pub mod cache;
 
 use ansi_term::Color::*;
 use indoc::indoc;
-use std::env;
-use std::process;
-use std::time::Instant;
+use std::{env, process, time::Instant};
 
 fn main() {
     //begin timing program execution
     let start = Instant::now();
+
+    //argument variables
+    let mut use_2lvl_cache;
+    let mut cache1_capacity = 0;
+    let mut cache2_capacity = 0;
+    let mut filepath = String::new();
+
+    //test metric variables
+    let mut num_refs1 = 0;
+    let mut num_refs2 = 0;
+    let mut num_hits1 = 0;
+    let mut num_hits2 = 0;
+    let mut num_hits_total = 0;
+    let mut hit_ratio1 = 0.0;
+    let mut hit_ratio2 = 0.0;
+    let mut hit_ratio_global = 0.0;
 
     //echo user input
     let args:Vec<String> = env::args().collect();
@@ -18,19 +34,33 @@ fn main() {
     }
     println!();
 
-    //validate args
+    //process args
     if !(4..=5).contains(&(args.len() as i32)) {
-        eprintln!("{}",Red.paint("Incorrect number of arguments provided"));
-        print_usage_and_exit();
+        print_usage_and_exit("Incorrect number of arguments provided");
+    }
+    cache1_capacity = args[2].parse::<u32>().unwrap_or_else(|ignored| print_usage_and_exit("Field 2 must be a positive integer"));
+    match args[1].as_str() {
+        "1" => {
+            use_2lvl_cache = false;
+            filepath = args[2].clone();
+        },
+        "2" => {
+            use_2lvl_cache = true;
+            cache2_capacity = args[3].parse::<u32>().unwrap_or_else(|ignored| print_usage_and_exit("Field 3 must be a positive integer"));
+            filepath = args[3].clone();
+        },
+        _ => {
+            print_usage_and_exit("Field 1 must be a 1 or 2");
+        }
     }
 
     //display total run time
     let elapsed = start.elapsed();
-    println!("Program execution took {}.{} seconds", elapsed.as_secs(), elapsed.subsec_millis());
+    println!("Program execution took {}.{:0<3} seconds", elapsed.as_secs(), elapsed.subsec_millis());
 }
 
-fn print_usage_and_exit() {
-    print!("{}",indoc! {"
+fn print_usage_and_exit(message:&str) -> u32 {
+    print!("{}\n{}", Red.paint(message), indoc! {"
 
         Usage:
             cargo run 1 <cache_size> <input_textfile_name>
@@ -38,4 +68,5 @@ fn print_usage_and_exit() {
 
     "});
     process::exit(1);
+    0
 }
